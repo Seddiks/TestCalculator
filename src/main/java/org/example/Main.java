af6a31097d5c0005292f9c4b2dfc26c72ebd21bf
Seddik Fredj <fredjseddik@gmail.com>
2023-01-26 00:34:00 +0100

    hola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! its from jenkins!!!!!!!!");
+        System.out.println("Hello world! its from jenkins seddikkkk!!!!!!!!");
     }
 }
